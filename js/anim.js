0be7d88c1127b2ecc131fcb44f7ce3571d2bcb85
IvoCarrascoMediaAgency <40969344+IvoCarrascoMediaAgency@users.noreply.github.com>
2020-02-02 12:13:56 -0500

    1

---
+++ js/anim.js
@@ -0,0 +1,8 @@
+var animation_fondo_cohete_home = bodymovin.loadAnimation({
+    container: document.getElementById('animism'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'json/animism.json',  
+    })
+
---
+++ js/anim2.js
@@ -0,0 +1,7 @@
+var animation_fondo_cohete_home = bodymovin.loadAnimation({
+    container: document.getElementById('animism2'),
+    renderer: 'svg',
+    loop: true,
+    autoplay: true,
+    path: 'json/animism2.json',  
+    })
